8783497f79764be08460b3ab6bf436faf12b5d24
Irini <irin.work42@gmail.com>
2018-07-17 18:17:30 +0300

    try..

--- server/config/config.js
+++ server/config/config.js
@@ -1,13 +1,13 @@
-const config = {
-  production: {
-    SECRET: process.env.SECRET,
-    DATABASE: process.env.MONGOLAB_URI
-  },
-  default: {
-    SECRET: 'SUPERSECRETPASSWORD123',
-    DATABASE: 'mongodb://localhost:27017/booksShelf'
-  }
-}
+// const config = {
+//   production: {
+//     SECRET: process.env.SECRET,
+//     DATABASE: process.env.MONGOLAB_URI
+//   },
+//   default: {
+//     SECRET: 'SUPERSECRETPASSWORD123',
+//     DATABASE: 'mongodb://localhost:27017/booksShelf'
+//   }
+// }
 
 // exports.get = function get(env){
 //   return config[env] || config.default
