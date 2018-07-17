31500af42416b2cad485b48eac36813991a89480
Irini <irin.work42@gmail.com>
2018-07-17 17:43:26 +0300

    comment config

--- server/config/config.js
+++ server/config/config.js
@@ -3,10 +3,10 @@ const config = {
     SECRET: process.env.SECRET,
     DATABASE: process.env.MONGODB_URI
   },
-  default: {
-    SECRET: process.env.SECRET,
-    DATABASE: process.env.MONGODB_URI
-  }
+  // default: {
+  //   SECRET: 'SUPERSECRETPASSWORD123',
+  //   DATABASE: 'mongodb://localhost:27017/booksShelf'
+  // }
 }
 
 exports.get = function get(env){
