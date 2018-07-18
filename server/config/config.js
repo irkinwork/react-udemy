a825c5f67ad4fc7f9a56bd88928d60a646461105
Irini <irin.work42@gmail.com>
2018-07-18 12:58:37 +0300

    change now

--- server/config/config.js
+++ server/config/config.js
@@ -1,14 +1,14 @@
 const config = {
   production: {
     DATABASE: process.env.MONGODB_URI
-  },
-  default: {
-    SECRET: 'SUPERSECRETPASSWORD123',
-    DATABASE: 'mongodb://localhost:27017/booksShelf'
-
   }
+  // default: {
+  //   SECRET: 'SUPERSECRETPASSWORD123',
+  //   DATABASE: 'mongodb://localhost:27017/booksShelf'
+
+  // }
 }
 
 exports.get = function get(env){
-  return config[env] || config.default
+  return config[env]
 }
